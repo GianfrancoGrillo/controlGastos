c693e3eecf2a24bc095529e27145cb3835adcf11
Gianfranco Grillo <gianfrancogrillo@Gianfrancos-MacBook-Air.local>
2023-03-29 12:26:11 -0300

    todo el proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
+
+const Header = ({
+  gastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+  setGastos
+
+}) => {
+
+  return (
+    <header>
+      <h1>PLANIFICADOR DE GASTOS</h1>
+
+      {isValidPresupuesto ? (
+        <ControlPresupuesto
+          gastos={gastos}
+          presupuesto={presupuesto}
+          setGastos={setGastos}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+
+        />
+      )}
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Gasto from './Gasto'
+
+const ListadoGastos = ({
+  gastos,
+  setGastoEditar,
+  eliminarGasto,
+  filtro,
+  gastoFiltro
+}) => {
+  return (
+    <div className='listado-gastos contenedor'>
+      
+      {filtro ? (
+        <>
+         <h2>{gastoFiltro.length ? 'Gastos' : 'No hay Gastos en esta categoria'}
+         </h2>
+        {gastoFiltro.map(gasto => (
+          <Gasto
+            key={gasto.id}
+            gasto={gasto}
+            setGastoEditar={setGastoEditar}
+            eliminarGasto={eliminarGasto}
+          />
+        ))
+        }</>
+      ) : (
+        <>
+         <h2>{gastos.length ? 'Gastos' : 'No hay Gastos'}
+         </h2>
+       { gastos.map(gasto => (
+          <Gasto
+            key={gasto.id}
+            gasto={gasto}
+            setGastoEditar={setGastoEditar}
+            eliminarGasto={eliminarGasto}
+          />
+      )) }
+      </>)
+    }
+    </div>
+
+  )
+}
+
+export default ListadoGastos
